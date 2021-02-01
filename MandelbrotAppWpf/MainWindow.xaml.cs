31094fc80ef01379fa1893d74a061dbf65faea1d
Alex <temper8@gmail.com>
2021-02-01 23:32:56 +0300

    test draw on skiaCanvas

--- MandelbrotAppWpf/MainWindow.xaml.cs
+++ MandelbrotAppWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,68 @@ namespace MandelbrotAppWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            InitBitmap(500, 500);
+        }
+
+        SKBitmap bitmap;
+        const string TEXT = "Hello, Bitmap!";
+        void InitBitmap(int w, int h)
+        {
+            // Create bitmap and draw on it
+            using (SKPaint textPaint = new SKPaint { TextSize = 48 })
+            {
+                SKRect bounds = new SKRect();
+                textPaint.MeasureText(TEXT, ref bounds);
+
+                bitmap = new SKBitmap(w, h);
+
+                using (SKCanvas bitmapCanvas = new SKCanvas(bitmap))
+                {
+                    bitmapCanvas.Clear();
+                    bitmapCanvas.DrawText(TEXT, w / 2 - bounds.Right / 2, h / 2 - bounds.Top, textPaint);
+                }
+            }
+        }
+        private void skiaCanvas_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
+        {
+            var surface = e.Surface;
+            var surfaceWidth = e.Info.Width;
+            var surfaceHeight = e.Info.Height;
+
+
+            var canvas = surface.Canvas;
+            /*
+            if (snapshot == null)
+            {
+                canvas.Clear();
+                planeView.Render(canvas);
+                snapshot = surface.Snapshot();
+                SaveSnapshot();
+            }
+            else
+            {
+                canvas.Clear();
+                canvas.DrawImage(snapshot, new SKPoint(0, 0));
+            }
+            */
+
+            var rect = SKRect.Create(10, 10, surfaceWidth - 20, surfaceHeight - 20);
+
+            // the brush (fill with blue)
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColors.AliceBlue
+            };
+
+
+            canvas.ResetMatrix();
+            //  canvas.Translate(dx, dy);
+            canvas.DrawRect(rect, paint);
+            canvas.DrawBitmap(bitmap, 20, 20);
+
+            canvas.Flush();
         }
     }
 }
